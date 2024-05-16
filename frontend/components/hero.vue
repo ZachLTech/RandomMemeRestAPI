c998b903fecf2bef132380f121631608e63503b3
ZachLTech <zlopezzl2021@gmail.com>
2024-05-16 19:19:41 -0400

    scaffold + before daisyUI

---
+++ frontend/components/hero.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col jusify-center items-center gap-5">
+        <img width="750" src="assets/static/laughing-emoji.svg" alt="" class="drop-shadow-xl brightness-50 absolute z-0">
+        <h1 class="text-6xl">Your #1 Meme Spot!</h1>
+        <p>A simple to use API for Memes which everyone most definitely needs lol</p>
+        <div class="flex gap-5">
+            <a href="#docs" class="">Try it</a>
+            <a href="https://github.com/ZachLTech/RandomMemeRestAPI">Source</a>
+        </div>
+    </div>
+</template>
