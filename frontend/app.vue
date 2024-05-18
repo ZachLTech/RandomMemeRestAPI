c6f5cce4fa0caed46a9bcb6eedd4452f4f9b4dc9
ZachLTech <zlopezzl2021@gmail.com>
2024-05-18 02:22:33 -0400

    testimonials

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,7 +2,7 @@
   <section id="hero" class="flex justify-center items-center h-[85vh]">
     <hero />
   </section>
-  <div id="demo" class="flex justify-center items-center">
+  <section id="demo" class="flex justify-center items-center">
     <div class="divider mx-5 w-[20vw]"></div>
     <div class="mockup-window border bg-base-300 w-[60vw] -translate-y-16">
       <div class="flex px-4 py-8 bg-base-200 h-[40vh]">
@@ -17,22 +17,26 @@
       </div>
     </div>
     <div class="divider mx-5 w-[20vw]"></div>
-  </div>
-  <section id="about" class="flex flex-col justify-center items-center gap-5">
-    <h1 class="text-5xl font-bold">Why Use This?</h1>
-    <p class="text-3xl max-w-[900px]">
-      Well in all honesty, this is more of a joke than a serious 
-      project as I'm sure you can tell. I originally just made the
-      API to learn ElysiaJS and get the hang of writing some 
-      backend API code, but was inspired to make a frontend
-      by the amazing <a href="https://capy.lol" target="_blank" class="underline text-accent">Capybara API</a> which I take much inspiration
-      from 😃
-    </p>
-    <div class="flex gap-5">
-      <testimonial />
-      <testimonial />
+  </section>
+  <section id="about" class="flex flex-col justify-center items-center gap-24 mt-24 mb-24">
+    <div class="flex flex-col gap-5 justify-center items-center">
+      <h1 class="text-5xl font-bold">Why Use This?</h1>
+      <p class="text-3xl max-w-[900px]">
+        In all honesty, this is more of a joke than a serious 
+        project as I'm sure you can tell. I originally just made the
+        API to learn ElysiaJS and get the hang of writing some 
+        backend API code, but was inspired to make a frontend
+        by the amazing <a href="https://capy.lol" target="_blank" class="underline text-accent">Capybara API</a> which I take much inspiration
+        from 😃
+      </p>
     </div>
-    <h1 class="text-7xl opacity-50">Testimonials</h1>
-    <testimonial />
+    <div class="flex flex-col gap-5 justify-center items-center">
+      <div class="flex gap-5">
+        <testimonial :width="'24vw'" :name="'Amarnath Pate 🐒'" :timeSince="'1 Decade'" :testimonialText="'had me cracking up all night long!!'" :imageSrc="'/images/amarnath.jpg'" />
+        <testimonial :width="'24vw'" :name="'Thandoid Manlez 🐱'" :timeSince="'5 Seconds'" :testimonialText="'whew! This brought some good laughter into my life!! meow'" :imageSrc="'/images/thandi.png'" />
+      </div>
+      <h1 class="text-9xl opacity-50">Testimonials</h1>
+      <testimonial :width="'49vw'" :name="'Jossaya Camel 🐪'" :timeSince="'8 Days'" :testimonialText="'Wowza this API was so great that it saved my entire family from falling apart. Truly amazing and resulted in my company avoiding bankruptcy!! 10/10 would recommend.'" :imageSrc="'/images/jossaya.png'" />
+    </div>    
   </section>
 </template>
--- frontend/components/hero.vue
+++ frontend/components/hero.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex flex-col justify-center items-center gap-5">
-        <img width="150" src="assets/static/laughing-emoji.svg" alt="" class="drop-shadow-xl z-0 transition-all hover:-translate-y-6">
+    <div class="flex flex-col justify-center items-center gap-5 h-[95vh]">
+        <img width="150" src="/laughing-emoji.svg" alt="" class="drop-shadow-xl z-0 transition-all hover:-translate-y-6">
         <h1 class="text-6xl font-bold">Your #1 Meme Spot!</h1>
         <p class="text-xl max-w-[500px] font-bold text-center">A simple to use API for Memes which everyone most definitely needs lol</p>
         <div class="flex gap-5">
-            <a href="#docs" class="btn w-24 btn-primary">Try it</a>
-            <a href="https://github.com/ZachLTech/RandomMemeRestAPI" target="_blank" class="btn w-24 btn-outline border-2 btn-primary">Source</a>
+            <a href="#docs" class="btn w-24 btn-secondary">Try it</a>
+            <a href="https://github.com/ZachLTech/RandomMemeRestAPI" target="_blank" class="btn w-24 border-2 btn-secondary bg-opacity-35">Source</a>
         </div>
     </div>  
 </template>
