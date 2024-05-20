2ab505cba5b52387c1bfc7b7e49b0180c7a608a7
ZachLTech <zlopezzl2021@gmail.com>
2024-05-20 06:33:14 -0400

    plausible

--- frontend/app/nuxt.config.ts
+++ frontend/app/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -9,5 +9,12 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@nuxtjs/device'],
+  modules: [
+    '@nuxtjs/device',
+    '@nuxtjs/plausible',
+  ],
+  
+  plausible: {
+    apiHost: 'https://analytics.lopezhome.tech',
+  },
 })
