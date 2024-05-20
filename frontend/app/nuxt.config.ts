2d4beffbbe878d6bfbcbbbe92dd6b008c25a3581
ZachLTech <zlopezzl2021@gmail.com>
2024-05-20 06:38:01 -0400

    SEO

--- frontend/app/nuxt.config.ts
+++ frontend/app/nuxt.config.ts
@@ -9,12 +9,16 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: [
-    '@nuxtjs/device',
-    '@nuxtjs/plausible',
-  ],
+  modules: ['@nuxtjs/device', '@nuxtjs/plausible', "@nuxtjs/seo"],
   
   plausible: {
     apiHost: 'https://analytics.lopezhome.tech',
   },
+
+  site: {
+    url: 'https://memes.zachl.space',
+    name: 'Meme API',
+    description: 'A simple to use API for Memes which everyone most definitely needs lol',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  }
 })
